290befb8408b3d0cdf364b55cb3aa94b85e1c0eb
Maria Snegireva <mariyasn88@gmail.com>
2023-05-04 00:10:47 +0300

    second commit

---
+++ removeElement.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ *
+ * @returns {number}
+ */
+function removeElement(nums, val) {
+  for (let i = 0; i <= nums.length; i++) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+
+  return nums.length;
+}
